f0c97eb3821d2eb1d8e33a266ba3966970cb5c80
Chris <631963997@qq.com>
2020-04-29 16:37:18 +0800

    form 第一版；

---
+++ src/components/CForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <el-form>
+      <c-form-item
+        v-for="(item,index) in items"
+        :key="item.type+index"
+        :item="item"
+        v-model="mode[item.formItemProp.prop]"
+      >
+      </c-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  name: "CForm",
+  components: {
+    CFormItem: () => import("./CFormItem")
+  },
+  props: {
+    mode: {
+      type: Object,
+      required: true
+    },
+    // {type:'',propertys:{}} type is elementUI form-item type
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  watch: {
+    mode(val) {
+      this.$emit("update:mode", val);
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {}
+};
+</script>
---
+++ src/components/CFormItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-form-item v-bind="item.formItemProp">
+      <component
+        :is="item.type"
+        v-model="value"
+        v-bind="item.attrs"
+        v-on="item.events"
+      >
+      <template v-if="item.components">
+          <div>
+              
+          </div>
+      </template>
+      </component>
+    </el-form-item>
+  </div>
+</template>
+<script>
+export default {
+  name: "CFormItem",
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      value: null
+    };
+  },
+  watch: {
+    value(val) {
+      this.$emit("input", val);
+    }
+  }
+};
+</script>
